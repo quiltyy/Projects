f428975bd8a133d50a2b3216d62867726529f34d
quiltyy <achamberlain008@gmail.com>
2021-05-19 12:58:05 -0700

    Created Project Workspace, Added JS Notes and Images for Logo

---
+++ javaScript/Truthy Falsy/app.js
@@ -0,0 +1,12 @@
+1 <= 4 && "a" === "a"; // true
+9 > 10 && 9 >= 9; // false
+
+let password = "chickenGal";
+
+if(password.length >= 8 && password.indexOf(' ') === -1){
+  console.log('Valid Password');
+}
+else {
+  console.log('Invalid Password'
+}
+
