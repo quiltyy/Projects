52456327cfc63ef6aac7a5ed44cf817ff1434e53
quiltyy <achamberlain008@gmail.com>
2021-05-20 23:47:18 -0700

    Arrays

---
+++ javaScript/Arrays and Objects/apps.js
@@ -0,0 +1,13 @@
+// Create an array
+let colors = ["blue", "red", "yellow", "green"];
+
+// Create an empty array
+let empty = [];
+
+// Last item of an array
+console.log(colors[colors.length - 1]);
+
+// Change the value of an array at the index given
+colors[1] = "torquoise";
+
+console.log(colors[1]);
