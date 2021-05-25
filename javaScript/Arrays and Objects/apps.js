f08bf3f5cbcd4b16c26c5ea7cac0302c2b64acb7
quiltyy <achamberlain008@gmail.com>
2021-05-25 15:38:49 -0700

    Complete Arrays and Objects built the Core of JS

--- javaScript/Arrays and Objects/apps.js
+++ javaScript/Arrays and Objects/apps.js
@@ -19,13 +19,13 @@ let topSongs = [
   "Life on Mars",
 ];
 
-// Push (returns the length of the array after addition)
+// ====== Push (returns the length of the array after addition)
 topSongs.push("Fortunate Son");
 
-// Pop (returns value that was removed from array)
+// ====== Pop (returns value that was removed from array)
 topSongs.pop();
 
-// Unshift (adds item to front of array and returns lengeth of array)
+// ====== Unshift (adds item to front of array and returns lengeth of array)
 let dishesaToDo = ["big platter"];
 dishesaToDo.unshift("large plate");
 dishesaToDo.unshift("small plate");
@@ -33,10 +33,10 @@ dishesaToDo.unshift("cereal bowl");
 dishesaToDo.unshift("mug");
 dishesaToDo.unshift("dirty spoon");
 
-// Shift (removes item from front of list and returns item)
+// ====== Shift (removes item from front of list and returns item)
 dishesaToDo.shift();
 
-// Concat
+// ====== Concat
 let fruits = ["apple", "banana"];
 let veggies = ["asparagus", "brussel sprouts"];
 let meats = ["steak", "chicken breast"];
@@ -45,7 +45,7 @@ console.log(fruits.concat(veggies));
 
 let allFood = fruits.concat(veggies, meats);
 
-//Includes and IndexOf
+// ======= Includes and IndexOf
 let ingredients = [
   "water",
   "corn starch",
@@ -60,17 +60,17 @@ let ingredients = [
 ingredients.includes("fish");
 if (ingredients.inclueds("water")) ingredients.indexOf("cheese");
 
-// Reverse
+// ======= Reverse
 // (Changes the actual variable.)
 let letters = ["T", "C", "E", "P", "S", "E", "R"];
 letters.reverse();
 
-// Join - Combines all the values of an array into one string.
+// ======== Join - Combines all the values of an array into one string.
 // Adding a value to the method inserts the value between each (Default is ,)
 letters.join();
 letters.join("&");
 
-//Slice
+// ======= Slice
 // Takes Portion of array and copy it into a new array
 let animals = ["shark", "salmon", "whale", "bear", "lizard", "tortoise"];
 
@@ -81,3 +81,39 @@ let reptiles = animals.slice(4);
 let quadrupeds = animals.slice(-3);
 // can call animals.slice(); to copy the entire array
 let copyOfAnimals = animals.slice();
+
+//  ======== Splice - Remove or Replace an element.
+// .splice(startIndex, deleteCount, itemsToInsert);
+let animals = ["shark", "salmon", "whale", "bear", "lizard", "tortoise"];
+
+// Inserting w/ Splice
+// At index 1, delete 0 items and insert octopus
+animals.splice(1, 0, "octopus");
+
+// Deleting w/ Splice
+// At index 5, delete 2 items
+animals.splice(3, 2);
+
+// Replacing w/ Splice
+// At index 3 delete 2 items and replace them with snake and frog
+animals.splice(3, 0, "snake", "frog");
+
+// ========== Sort
+// Updates and replaces the array with the changed order
+let people = ["Mrs. Robinson", "Angie", "Jolene", "Maggie May", "Roxanne"];
+people.sort();
+
+// Covert everything to string then sort
+// Is Not a Numeric Sort
+let nums = [34, 10, 10000, 67, 99];
+nums.sort();
+
+// =====  Nested Arrays
+const animalPairs = [
+  ["doe", "buck"],
+  ["ewe", "ram"],
+  ["peaham", "peacock"],
+];
+
+consol.log(animalPairs[2][0]); // Returns peaham
+animalPairs[0][1] = "stag"; // Changes buck to stag
