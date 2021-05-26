091265a98d31bea9ae64b5e8a6c8b763e87375c1
quiltyy <achamberlain008@gmail.com>
2021-05-26 13:19:48 -0700

    For Loops

--- javaScript/The World of Loops/app.js
+++ javaScript/The World of Loops/app.js
@@ -1,10 +1,62 @@
-const test = {
-  "AP BIO": 120,
-  GYM: 90,
-};
+// ===== FOR LOOPS
+// for ([initialExpression]; [condition]; [incrementExpression])
+// for (let i = 1; i <= 10; i++) {
+//   console.log("HELLO:", i);
+// }
 
-let pine = "pine";
+// for (let num = 1; num <= 20; num++) {
+//   console.log(`${num}x${num} = ${num * num}`);
+// }
 
-let apple = "apple";
+for (let i = 200; i >= 0; i -= 25) {
+  console.log(i);
+}
 
-console.log(pine + apple);
+// INFINITE LOOPS (Don't run this)
+// for (let i = 20; i >= 0; i++){
+//  console.log(i);
+// }
+
+// FOR LOOPS & ARRAYS
+const animals = ["lions", "tigers", "bears"];
+for (let i = 0; i < animals.length; i++) {
+  console.log(i, animals[i]);
+}
+
+const myStudents = [
+  {
+    firstName: "Zeus",
+    grade: 86,
+  },
+  {
+    firstName: "Artemis",
+    grade: 97,
+  },
+  {
+    firstName: "Hera",
+    grade: 72,
+  },
+  {
+    firstName: "Apollo",
+    grade: 90,
+  },
+];
+
+for (let i = 0; i < myStudents.length; i++) {
+  let student = myStudents[i];
+  console.log(`${student.firstName} scored ${student.grade}`);
+}
+
+// If looping in reverse i = variable.length - 1; i >= 0 ; i --
+const word = "stressed";
+for (let i = word.length - 1; i >= 0; i--) {
+  console.log(word[i]);
+}
+
+let sumOfGrades = 0;
+for (let i = 0; i < myStudents.length; i++) {
+  let student = myStudents[i];
+  sumOfGrades += student.grade;
+}
+
+console.log(sumOfGrades / myStudents.length);
