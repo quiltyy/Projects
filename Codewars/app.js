820f426086a92936f5fbabe35aa4a55c22bcc091
quiltyy <achamberlain008@gmail.com>
2021-06-08 22:45:53 -0700

    Codewars

--- Codewars/app.js
+++ Codewars/app.js
@@ -1,13 +1,20 @@
-// My Solution
-function repeatStr(n, s) {
-  let str = "";
-  for (let i = 0; i < n; i++) {
-    str += s;
-  }
-  return str;
-}
+// CHECK IF NUMBER EVEN OR ODD
+// function even_or_odd(number) {
+//   ...
+//   return number % 2 === 0 ? "even" : "odd";
+// }
 
-// Best Solution
-function repeatStr(n, s) {
-  return s.repeat(n);
-}
+// CHECK STRING FOR CERTAIN LETTERS
+// function getCount(str) {
+//   var vowelsCount = 0;
+//   var vowels = ["a","e","i","o","u"];
+//   for(var i = 0;i < str.length;i++){
+//     for(var j=0;j<vowels.length;j++){
+//       if(str[i] === vowels[j]){
+//         vowelsCount++;
+//       }
+//     }
+//   }
+
+//   return vowelsCount;
+// }
