51c79a230a23618884111e18c26600b743b6eb8a
quiltyy <achamberlain008@gmail.com>
2021-06-20 21:15:06 -0700

    forEach

--- Apply Functions/app.js
+++ Apply Functions/app.js
@@ -1,1 +1,24 @@
 // === Array Callback Methods ===
+// forEach
+const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+nums.forEach(function (n) {
+    console.log(n + n)
+});
+
+nums.forEach(function (el) {
+    if (el % 2 === 0) {
+        console.log(el)
+    }
+})
+
+nums.forEach(function (num, idx) {
+    console.log(idx, num);
+})
+
+
+// map
+// filter
+// find 
+// reduce
+//some
+// every
