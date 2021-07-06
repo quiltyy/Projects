289e4e9bec3272ac787cf0583702997c6f2a0681
quiltyy <achamberlain008@gmail.com>
2021-07-06 15:11:27 -0700

    Added Shuffle to the Deck Method

--- javaScript/Deck of Cards/app.js
+++ javaScript/Deck of Cards/app.js
@@ -1,24 +1,6 @@
-function makeDeck() {
-    const deck = [];
-    const suits = ['hearts', 'spades', 'clubs', 'diamonds'];
-    const values = '2,3,4,5,6,7,8,9,10,J,Q,K,A';
-    for (let value of values.split(',')) {
-        for (let suit of suits) {
-            deck.push({
-                values: values,
-                suit: suit
-            })
-        }
-    }
-    return deck;
-}
-
-function drawCard(deck) {
-    return deck.pop()
-}
-
 const myDeck = {
     deck: [],
+    drawnCards: [],
     suits: ['hearts', 'spades', 'clubs', 'diamonds'],
     values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
     insitalizeDeck() {
@@ -38,6 +20,30 @@ const myDeck = {
 
     },
     drawCard() {
-        return this.deck.pop()
+        // draw a single card from the top of the deck
+        const card = this.deck.pop();
+        this.drawnCards.push(card);
+        return card;
+    },
+    drawMultiple(numCards) {
+        // select a number of cards from the top of the deck
+        const cards = [];
+        for (let i = 0; i < numCards; i++) {
+            cards.push(this.drawCard());
+        }
+        return cards;
+    },
+    shuffle() {
+        const {
+            deck
+        } = this;
+        //loop over deck backwards
+        for (let i = deck.length - 1; i > 0; i--) {
+            //pick rnadom index beflore current element
+            let j = Math.floor(Math.random() * (i + 1));
+            // swap
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+
     }
 }
