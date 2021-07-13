e10cb99dad36e1417e3e6a87b9b7c90496e90e97
Andrew Chamberlain <achamberlain008@gmail.com>
2021-07-12 21:20:29 -0700

    innerHTML and innerText

---
+++ javaScript/Dom Manipulation/app.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form')
+
+form.innerHTML
+
+const ul = document.querySelector('ul')
+
+ul.innerText
+
+ul.innerHTML
+
+const h1 = document.querySelector9('h1');
