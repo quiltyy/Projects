630ee0eef13e517647a2d25da338f9d51b51ea16
Andrew Chamberlain <achamberlain008@gmail.com>
2021-07-12 22:07:54 -0700

    Value and Src

--- javaScript/Dom Manipulation/app.js
+++ javaScript/Dom Manipulation/app.js
@@ -9,3 +9,17 @@ ul.innerText
 ul.innerHTML
 
 const h1 = document.querySelector9('h1');
+
+const input = document.querySelectorAll('input')
+
+inputs[0].value
+inpust[1].value
+inputs[2].checked
+input[1].placeholder = ' '
+
+const a = document.querySelector('a')
+
+a.href
+
+const img = document.querySelector('img')
+img.src
