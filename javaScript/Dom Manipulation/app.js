2cf7b3ca5655219d4f1e883e8ba214c33a107a77
Andrew Chamberlain <achamberlain008@gmail.com>
2021-07-22 23:01:55 -0700

    Changings Multiple Elements.

--- javaScript/Dom Manipulation/app.js
+++ javaScript/Dom Manipulation/app.js
@@ -1,43 +1,53 @@
-// HEAD
-const form = document.querySelector('form')
+// // HEAD
+// const form = document.querySelector('form')
 
-form.innerHTML
+// form.innerHTML
 
-const ul = document.querySelector('ul')
+// const ul = document.querySelector('ul')
 
-ul.innerText
+// ul.innerText
 
-ul.innerHTML
+// ul.innerHTML
 
-const h1 = document.querySelector9('h1');
+// const h1 = document.querySelector9('h1');
 
-const input = document.querySelectorAll('input')
+// const input = document.querySelectorAll('input')
 
-inputs[0].value
-inpust[1].value
-inputs[2].checked
-input[1].placeholder = ' '
+// inputs[0].value
+// inpust[1].value
+// inputs[2].checked
+// input[1].placeholder = ' '
 
-const a = document.querySelector('a')
+// const a = document.querySelector('a')
 
-a.href
+// a.href
 
-const img = document.querySelector('img')
-img.src
-// =======
-const sheet = new CSSStyleSheet();
-sheet.replaceSync('*  {transition: all 2s}');
-// Have to change an Element to ID "viewport"
+// const img = document.querySelector('img')
+// img.src
+// // =======
+// const sheet = new CSSStyleSheet();
+// sheet.replaceSync('*  {transition: all 2s}');
+// // Have to change an Element to ID "viewport"
 
-document.adoptedStyleSheets = [sheet];
+// document.adoptedStyleSheets = [sheet];
 
-const allElls = document.getElementById("viewport").children;
+// const allElls = document.getElementById("viewport").children;
 
-setInterval(() => {
-    for (let el of allElls) {
-        const rotation = Math.floor(Math.random() * 360);
-        const x = Math.floor(document.body.clientWidth * Math.random())
-        const y = Math.floor(document.body.clientHeight * Math.random())
-        el.style.transform = `translate(${x}px, ${y}px) rotate(${rotation}deg`;
-    }
-}, 2000)
+// setInterval(() => {
+//     for (let el of allElls) {
+//         const rotation = Math.floor(Math.random() * 360);
+//         const x = Math.floor(document.body.clientWidth * Math.random())
+//         const y = Math.floor(document.body.clientHeight * Math.random())
+//         el.style.transform = `translate(${x}px, ${y}px) rotate(${rotation}deg`;
+//     }
+// }, 2000)
+
+const allLis = document.querySelectorAll('li');
+
+// for (let i = 0; i < allLis.length; i++) {
+//     console.log(allLis[i].innerText);
+// }
+
+for (let li of allLis) {
+    li.innerHTML = 'WE ARE <b>THE CHAMBERLAINS</b>'
+}
