77e11fb79870e12f5f0ee7216298bd2b102e0c00
Andrew Chamberlain <33704669+quiltyy@users.noreply.github.com>
2021-09-02 16:30:48 -0700

    More Promises

--- javaScript/Promise/app.js
+++ javaScript/Promise/app.js
@@ -1,16 +1,37 @@
-const willGetYouADog = new Promise((resolve, reject) => {
-    const rand = Math.random();
-    if (rand < 0.5) {
-        resolve();
-    } else {
-        reject();
-    }
-});
+// const willGetYouADog = new Promise((resolve, reject) => {
+//     const rand = Math.random();
+//     if (rand < 0.5) {
+//         resolve();
+//     } else {
+//         reject();
+//     }
+// });
 
-willGetYouADog.then(() => {
-    console.log('Yay We Got a Dog!!!');
-});
+// willGetYouADog.then(() => {
+//     console.log('Yay We Got a Dog!!!');
+// });
 
-willGetYouADog.catch(() => {
-    console.log('No Dog');
-})
+// willGetYouADog.catch(() => {
+//     console.log('No Dog');
+// })
+
+const makeDogPromise = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const rand = Math.random();
+            if (rand < 0.5) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, 5000)
+    });
+};
+
+makeDogPromise()
+    .then(() => {
+        console.log('Yay We Got a Dog!!!');
+    })
+    .catch(() => {
+        console.log('No Dog');
+    });
