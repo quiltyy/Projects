729003b2f9fef65c918d9d113da78c6d6f2b7847
Andrew Chamberlain <achamberlain008@gmail.com>
2021-09-08 21:37:18 -0700

    Fixed Commenting

--- Codewars/app.js
+++ Codewars/app.js
@@ -1,84 +1,84 @@
 // ====== CHECK IF NUMBER EVEN OR ODD
-// function even_or_odd(number) {
-//   ...
-//   return number % 2 === 0 ? "even" : "odd";
-// }
+function even_or_odd(number) {
+  ...
+  return number % 2 === 0 ? "even" : "odd";
+}
 
 // ====== CHECK STRING FOR CERTAIN LETTERS
-// function getCount(str) {
-//   var vowelsCount = 0;
-//   var vowels = ["a","e","i","o","u"];
-//   for(var i = 0;i < str.length;i++){
-//     for(var j=0;j<vowels.length;j++){
-//       if(str[i] === vowels[j]){
-//         vowelsCount++;
-//       }
-//     }
-//   }
-//   return vowelsCount;
-// }
+function getCount(str) {
+  var vowelsCount = 0;
+  var vowels = ["a","e","i","o","u"];
+  for(var i = 0;i < str.length;i++){
+    for(var j=0;j<vowels.length;j++){
+      if(str[i] === vowels[j]){
+        vowelsCount++;
+      }
+    }
+  }
+  return vowelsCount;
+}
 
 // ====== CONVERT NUMBERS TO STRING
-// function numberToString(num) {
-//     return num.toString();
-//   }
+function numberToString(num) {
+    return num.toString();
+  }
 
 // ====== REMOVE SPACES FROM STRING
-// function noSpace(x){
-//     combine = x.split(' ').join('');
-//     return combine
-//   }
+function noSpace(x){
+    combine = x.split(' ').join('');
+    return combine
+  }
 
 // ====== GET RANDOM INTEGER
-// function getRandomInt(max) {
-//   return Math.floor(Math.random() * max);
-// }
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 // ====== RUN FUNCTION X TIMES
-// function deal(size) {
-//   for (let i = 0; i <= size - 1; i++) {
-//     getCard();
-//   }
-// }
+function deal(size) {
+  for (let i = 0; i <= size - 1; i++) {
+    getCard();
+  }
+}
 
 // ====== DOUBLE AN ARRAY
-// function doubleArr(arr) {
-//   const result = [];
-//   for (let num of arr) {
-//     let double = num * 2;
-//     result.push(double);
-//   }
-//   return result;
-// }
+function doubleArr(arr) {
+  const result = [];
+  for (let num of arr) {
+    let double = num * 2;
+    result.push(double);
+  }
+  return result;
+}
 
 // ====== REVERSE WORDS IN A STRING BUT NOT ORDER
-// function reverseWords(str) {
-//   // Go for it
-//   const final = [];
-//   for (let word of str.split(" ")) {
-//     final.push(word.split("").reverse().join(""));
-//   }
-//   return final.join(" ");
-// }
+function reverseWords(str) {
+  // Go for it
+  const final = [];
+  for (let word of str.split(" ")) {
+    final.push(word.split("").reverse().join(""));
+  }
+  return final.join(" ");
+}
 
 
-// function nbYear(p0, percent, aug, p) {
-//     let i = 0;
-//     percent = percent / 100;
-//     console.log(percent);
-//     while (p0 < p) {
-//       p0 += p0 * percent + aug;
-//       i++
-//     }
-//     return i
+function nbYear(p0, percent, aug, p) {
+    let i = 0;
+    percent = percent / 100;
+    console.log(percent);
+    while (p0 < p) {
+      p0 += p0 * percent + aug;
+      i++
+    }
+    return i
 
-//     // your code
-//   }
+    // your code
+  }
 
 
 // ======= IS THE INTEGER A SQUARE
-// var isSquare = x => {
-//     return Math.sqrt(x) % 1 === 0
-// }
+var isSquare = x => {
+    return Math.sqrt(x) % 1 === 0
+}
 
-// isSquare(9)
+isSquare(9)
