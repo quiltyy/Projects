cb969aaf59435f193d62dff2220e7ac731658969
Andrew Chamberlain <33704669+quiltyy@users.noreply.github.com>
2021-10-14 12:23:14 -0700

    Reload on click and remove highlighting

--- Kanye8 Ball/app.js
+++ Kanye8 Ball/app.js
@@ -1,4 +1,15 @@
-axios.get("https://api.kanye.rest/").then((response) => {
-  quote = response.data.quote;
-  document.getElementById("quote").innerHTML = `${quote}`;
-});
+async function getQuote() {
+  const res = await axios.get("https://api.kanye.rest/");
+  quote = res.data.quote;
+  document.getElementById("kanye8").innerHTML = `${quote}`;
+}
+
+// getQuote().then((res) => {
+//   quote = res.data.quote;
+//   document.getElementById("quote").innerHTML = `${quote}`;
+// });
+
+window.onclick = reloadQuote;
+function reloadQuote() {
+  getQuote();
+}
