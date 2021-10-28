efd29a19a9f3fa5d4b1ca88f2e87086f86f2110b
Andrew Chamberlain <33704669+quiltyy@users.noreply.github.com>
2021-10-28 12:49:20 -0700

    Pet class

--- javaScript/6. JS Classes/app.js
+++ javaScript/6. JS Classes/app.js
@@ -21,4 +21,31 @@ class Color {
   }
 }
 
-const c1 = new Color(255, 67, 89, "tomato");
+class Pet {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  eat() {
+    return `${this.name} is eating!`;
+  }
+}
+
+class Cat extends Pet {
+  constructor(name, age, livesLeft = 9) {
+    super(name, age); // Refers to the Pet constructor to pull insto this class for this constructor
+    this.livesLeft = livesLeft;
+  }
+  meow() {
+    return `MEOWWW!!`;
+  }
+}
+
+class Dog extends Pet {
+  bark() {
+    return "WOOF!!";
+  }
+  eat() {
+    return `${this.name} scarfs his food!`;
+  }
+}
