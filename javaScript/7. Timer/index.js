8a21edc1868f42940d78c1ad3dccf265790dd01e
Andrew Chamberlain <achamberlain008@gmail.com>
2021-10-28 21:45:45 -0700

    Timer

---
+++ javaScript/7. Timer/index.js
@@ -0,0 +1,11 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+  }
+  start() {}
+  pause() {}
+  onDurationChange() {}
+  tick() {}
+}
