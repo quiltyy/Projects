c1156f193a3f614989276f916e715b41f431ba0c
Andrew Chamberlain <33704669+quiltyy@users.noreply.github.com>
2021-10-29 12:13:42 -0700

    start and pause

--- javaScript/7. Timer/index.js
+++ javaScript/7. Timer/index.js
@@ -3,9 +3,26 @@ class Timer {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
   }
-  start() {}
-  pause() {}
-  onDurationChange() {}
-  tick() {}
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  tick = () => {
+    console.log("tick");
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
 }
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);
