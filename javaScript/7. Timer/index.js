3045df8af4495d60ac3a36ace7f22094711e04df
Andrew Chamberlain <33704669+quiltyy@users.noreply.github.com>
2021-11-08 15:29:58 -0800

    Circle clears by 0

--- javaScript/7. Timer/index.js
+++ javaScript/7. Timer/index.js
@@ -2,18 +2,19 @@ const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 const circle = document.querySelector("circle");
-
 const perimeter = circle.getAttribute("r") * 2 * Math.PI;
 circle.setAttribute("stroke-dasharray", perimeter);
 
-let currentOffset = 0;
+let duration;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
-    console.log("Timer started");
+  onStart(totalDuration) {
+    duration = totalDuration;
   },
-  onTick() {
-    circle.setAttribute("stroke-dashoffset", currentOffset);
-    currentOffset = currentOffset - 50;
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      "stroke-dashoffset",
+      (perimeter * timeRemaining) / duration - perimeter
+    );
   },
   onComplete() {
     console.log("Timer is completed");
