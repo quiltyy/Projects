9ef2082af14fc628e9f16d679078dc74023c9a1f
Andrew Chamberlain <achamberlain008@gmail.com>
2021-11-10 16:27:22 -0800

    Updated auto-complete Element

--- javaScript/8. Movie Fight/index.js
+++ javaScript/8. Movie Fight/index.js
@@ -31,18 +31,33 @@ const resultsWrapper = document.querySelector(".results");
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
 
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
+
   resultsWrapper.innerHTML = "";
   dropdown.classList.add("is-active");
   for (let movie of movies) {
     const option = document.createElement("a");
-    const imgSRC = movie.Poster === "N/A" ? "" : item.Poster;
+    const imgSRC = movie.Poster === "N/A" ? " " : movie.Poster;
     option.classList.add("dropdown-item");
     option.innerHTML = `
       <img src="${imgSRC}" />
       ${movie.Title}
     `;
+    option.addEventListener("click", () => {
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+    });
 
     resultsWrapper.appendChild(option);
   }
 };
 input.addEventListener("input", debounce(onInput, 500));
+
+document.addEventListener("click", (event) => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove("is-active");
+  }
+});
