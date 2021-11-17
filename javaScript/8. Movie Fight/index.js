b1e9a1b7487fb514370218d10bd1e01d60a9287d
Andrew Chamberlain <33704669+quiltyy@users.noreply.github.com>
2021-11-17 14:49:29 -0800

    Pull Down extra Data

--- javaScript/8. Movie Fight/index.js
+++ javaScript/8. Movie Fight/index.js
@@ -40,15 +40,17 @@ const onInput = async (event) => {
   dropdown.classList.add("is-active");
   for (let movie of movies) {
     const option = document.createElement("a");
-    const imgSRC = movie.Poster === "N/A" ? " " : movie.Poster;
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+
     option.classList.add("dropdown-item");
     option.innerHTML = `
-      <img src="${imgSRC}" />
+      <img src="${imgSrc}" />
       ${movie.Title}
     `;
     option.addEventListener("click", () => {
       dropdown.classList.remove("is-active");
       input.value = movie.Title;
+      onMovieSelect(movie);
     });
 
     resultsWrapper.appendChild(option);
@@ -69,4 +71,47 @@ const onMovieSelect = async (movie) => {
       i: movie.imdbID,
     },
   });
+
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+
+    
+  `;
 };
