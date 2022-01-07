fa7c9d23c939b1f481624b7716f3c27971dfd7e0
Andrew Chamberlain <achamberlain008@gmail.com>
2022-01-06 18:33:38 -0800

    Started App

--- javaScript/11. NodeJS/index.js
+++ javaScript/11. NodeJS/index.js
@@ -1,8 +1,1 @@
-const counterObject = require("./myscript.js");
-
-console.log(counterObject.getCounter());
-counterObject.incrementCounter();
-console.log(counterObject.getCounter());
-
-const newCounterObject = require("./myscript.js");
-console.log(newCounterObject.getCounter());
+const fs = require("fs");
