e6257aa864669b9925c8e5a49f2f1d393ae4cdde
Andrew Chamberlain <achamberlain008@gmail.com>
2022-01-07 16:10:13 -0800

    Callback to Promises

--- javaScript/11. NodeJS/index.js
+++ javaScript/11. NodeJS/index.js
@@ -1,1 +1,29 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
+
+// METHOD 2
+// const util = require("util");
+// const lstat = until.promisify(fs.lstat);
+
+// METHOD 3
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), (err, filenames) => {
+  if (err) {
+    // error handling code here
+    console.log(err);
+  }
+});
+
+// METHOD 1
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(stats);
+//     });
+//   });
+// };
