02aeba6defdf3c1f533cbf0716790cac0f64f92f
Quilty <achamberlain008@gmail.com>
2022-04-20 13:14:56 -0700

    python class

---
+++ python/forLoops.py
@@ -0,0 +1,8 @@
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+
+print('My name is')
+for i in range(0, 10):
+    print('Jimmy Five Times ' + str(i))
---
+++ python/modules.py
@@ -0,0 +1,8 @@
+import random
+import sys
+import os
+import math
+print(random.randint(1, 100))
+print('Hello')
+sys.exit()
+print('Goodybe')
