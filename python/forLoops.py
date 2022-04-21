0f2138780d386f8b2e73d3c99c07a39399c5527e
Andrew Chamberlain <achamberlain008@gmail.com>
2022-04-20 22:58:26 -0700

    formatting

--- python/forLoops.py
+++ python/forLoops.py
@@ -3,6 +3,6 @@ for num in range(101):
     total = total + num
 print(total)
 
-print('My name is')
+print("My name is")
 for i in range(0, 10):
-    print('Jimmy Five Times ' + str(i))
+    print("Jimmy Five Times " + str(i))
