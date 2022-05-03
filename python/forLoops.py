a2429cf77c3236897fc0585da4f6f4b8d8fa2515
Quilty <achamberlain008@gmail.com>
2022-05-02 18:23:43 -0700

    own functions

--- python/forLoops.py
+++
@@ -1,8 +0,0 @@
-total = 0
-for num in range(101):
-    total = total + num
-print(total)
-
-print('My name is')
-for i in range(0, 10):
-    print('Jimmy Five Times ' + str(i))
--- python/modules.py
+++
@@ -1,9 +0,0 @@
-import random
-import sys
-import os
-import math
-
-print(random.randint(1, 100))
-print('Hello')
-sys.exit()
-print('Goodybe')
