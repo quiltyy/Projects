f79e3d24239e97522b66469ec905f5c64f015bc5
Quilty <achamberlain008@gmail.com>
2022-06-28 00:50:03 -0700

    n

--- python/LTCWFF/Andrew/test.py
+++ python/LTCWFF/Andrew/test.py
@@ -1,4 +1,29 @@
-# test
-print('Andrew')
+my_roster_list = ['tom brady', 'adrian peterson', 'antonio brown']
+my_roster_list_proper = [x.title() for x in my_roster_list]
+print(my_roster_list)
+print(my_roster_list_proper)
 
-# j
+my_roster_last_name = [full_name.split(
+    ' ')[1].title() for full_name in my_roster_list]
+print(my_roster_last_name)
+
+pts_per_player = {
+    'tom brady': 20.7, 'adrian peterson': 10.1, 'antonio brown': 18.5
+}
+
+pts_x2_per_upper_player = {
+    name.title(): pts*2 for name, pts in pts_per_player.items()
+}
+print(pts_per_player)
+print(pts_x2_per_upper_player)
+print(sum([pts for _, pts in pts_per_player.items()]))
+
+
+def rec_pts(yds, rec, tds):
+    """
+    this function takes number of receiving: yards, receptions and touchdown and returns fantasy points scored (ppr scoring)
+    """
+    print(yds*0.1 + rec*1 + tds*6)
+
+
+rec_pts(110, 6, 1)
