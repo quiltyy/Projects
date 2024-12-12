3458e09a25b72f5df948fa2ef37eecf28e227da6
quiltyy <achamberlain008@gmail.com>
2024-12-11 21:39:57 -0800

    ChoreGold

--- ChoreGold/ChoreGold/ViewController.swift
+++ ChoreGold/ChoreGold/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Outlets
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var continueButton: UIButton!
+    @IBOutlet weak var stepper: UIStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Set the initial values for the label and stepper
+        counterLabel.text = "0"
+        stepper.value = 0
+        stepper.minimumValue = 0 // Optional to prevent negative values
     }
-
-
+    // IBAction for the stepper
+    @IBAction func pressStepper(_ sender: Any) {
+        // Update the label with the current value of the stepper
+        counterLabel.text = String(Int(stepper.value))
+        
+    }
+    @IBAction func continuePressed(_ sender: Any) {
+    }
+    
 }
 
