00c8d884adf883cc35a46a8d8b15827927fb3b60
quiltyy <achamberlain008@gmail.com>
2024-12-11 16:59:52 -0800

    Gold Star

---
+++ Gold Star/Gold Star/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gold Star
+//
+//  Created by Andrew Chamberlain on 12/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Gold Star/Gold StarTests/Gold_StarTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gold_StarTests.swift
+//  Gold StarTests
+//
+//  Created by Andrew Chamberlain on 12/5/24.
+//
+
+import Testing
+@testable import Gold_Star
+
+struct Gold_StarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
