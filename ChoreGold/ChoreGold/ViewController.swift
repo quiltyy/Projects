0e399589838500b970510adb3c792138ce42d424
quiltyy <achamberlain008@gmail.com>
2024-12-17 23:13:33 -0800

    erk

--- ChoreGold/ChoreGold/ViewController.swift
+++ ChoreGold/ChoreGold/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    // Outlets
+    @IBOutlet weak var textEntryField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    // Data source for the table
+    var tableData: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Set delegate and dataSource
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    // Button action
+    @IBAction func addButtonTapped(_ sender: UIButton) {
+        // Ensure text is not empty
+        guard let text = textEntryField.text, !text.isEmpty else { return }
+        // Add text to the data source
+        tableData.append(text)
+        // Clear the text field
+        textEntryField.text = ""
+        // Reload table view
+        tableView.reloadData()
+    }
 
+    // MARK: - Table View Data Source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") ?? UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = tableData[indexPath.row]
+        return cell
+    }
 }
 
